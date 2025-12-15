b8073af7ae8180ea1c020ee142ea3ad856c65720
Rutuja Pawar <rrutujapawar11@gmail.com>
2025-12-15 07:22:53 +0530

    assign1

---
+++ assign1/5.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    char x;
+    printf("Enter the character:");
+    scanf("%c",&x);
+    if((x>='A' && x<='Z') ||  ( x>='a' && x<='z'))
+    {
+        printf("Alphabets are present!!\n");
+        //and
+        int y=x^32;
+        printf("%d",y);  
+    }
+    else
+    {
+        printf("Alphabets are not present!!");
+    }
+}
